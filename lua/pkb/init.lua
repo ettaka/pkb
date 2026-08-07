1b2603eeb95ea90e94527e3193e21e44f2d24e86
Eelis Takala <eelis.takala@gmail.com>
2026-08-07 16:04:01 +0300

    add notifier

--- lua/pkb/init.lua
+++ lua/pkb/init.lua
@@ -1,6 +1,10 @@
 local M = {}
 
+local notifier = require("pkb.notifier")
+
 function M.setup(opts)
+  -- If you add a config module later, you can pass options here
+  notifier.start_timer()
   vim.notify("PKB plugin loaded successfully!", vim.log.levels.INFO)
 end
 
--- plugin/pkb.lua
+++ plugin/pkb.lua
@@ -1,3 +1,9 @@
-vim.api.nvim_create_user_command("PKBHello", function()
-  require("pkb").setup()
+require("pkb").setup()
+
+vim.api.nvim_create_user_command("PKBNotify", function()
+  require("pkb.notifier").notify()
+end, {})
+
+vim.api.nvim_create_user_command("PKBInbox", function()
+  require("pkb.notifier").inbox()
 end, {})
