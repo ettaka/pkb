02860e19c813647481b0a09da417f3261b5dd593
Eelis Takala <eelis.takala@gmail.com>
2026-08-07 14:16:31 +0300

    feat: initial plugin scaffold

---
+++ README.md
@@ -0,0 +1,3 @@
+# pkb.nvim
+
+A simple Neovim plugin template for PKB.
---
+++ lua/pkb/init.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.setup(opts)
+  vim.notify("PKB plugin loaded successfully!", vim.log.levels.INFO)
+end
+
+return M
