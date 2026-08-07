5d11dd2bf949e1cb4a2580791ee5f8819ce8cbd1
Eelis Takala <eelis.takala@gmail.com>
2026-08-07 16:28:00 +0300

    remove nil call

--- lua/pkb/init.lua
+++ lua/pkb/init.lua
@@ -4,7 +4,6 @@ local notifier = require("pkb.notifier")
 
 function M.setup(opts)
   -- If you add a config module later, you can pass options here
-  notifier.start_timer()
   vim.notify("PKB plugin loaded successfully!", vim.log.levels.INFO)
 end
 
